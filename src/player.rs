59aec1c8599bf204897636a0033e1d3f9929c6fb
Zan Stopar <zan.stopar@jkaref.com>
2024-09-20 10:23:38 +0200

    add: player systems

--- src/player.rs
+++ src/player.rs
@@ -12,8 +12,9 @@ pub struct Player;
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Playing), spawn_player)
-            .add_systems(Update, move_player.run_if(in_state(GameState::Playing)));
+        app.add_systems(OnEnter(GameState::Playing), spawn_or_show_player)
+            .add_systems(Update, move_player.run_if(in_state(GameState::Playing)))
+            .add_systems(OnExit(GameState::Playing), hide_player);
     }
 }
 
@@ -25,6 +26,33 @@ fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
             ..Default::default()
         })
         .insert(Player);
+}
+
+fn despawn_player(mut commands: Commands, query: Query<Entity, With<Player>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
+    }
+}
+
+fn hide_player(mut commands: Commands, query: Query<Entity, With<Player>>) {
+    for entity in query.iter() {
+        commands.entity(entity).insert(Visibility::Hidden);
+    }
+}
+
+
+fn show_player(mut commands: Commands, query: Query<Entity, With<Player>>) {
+    for entity in query.iter() {
+        commands.entity(entity).insert(Visibility::Visible);
+    }
+}
+
+fn spawn_or_show_player(commands: Commands, query: Query<Entity, With<Player>>, textures: Res<TextureAssets>) {
+    if query.iter().next().is_none() {
+        spawn_player(commands, textures);
+    } else {
+        show_player(commands, query);
+    }
 }
 
 fn move_player(
